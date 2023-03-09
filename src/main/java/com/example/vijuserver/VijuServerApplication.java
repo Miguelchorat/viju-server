0ca7609a628f7ec75c8c1cdeff1319511cb2fdd2
Miguel Angel Chorat Delgado <miguelchorat@gmail.com>
2023-03-09 17:41:59 +0100

    Initial commit

---
+++ src/main/java/com/example/vijuserver/VijuServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.vijuserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VijuServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VijuServerApplication.class, args);
+    }
+
+}
